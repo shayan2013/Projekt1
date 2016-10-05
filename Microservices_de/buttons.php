ee69a56400719b0db4d69033e63301c536668d5f
Shayan Shams <Shayan Shams>
2016-10-05 11:18:36 +0200

    add to do comments to buttons.php

---
+++ Microservices_de/buttons.php
@@ -0,0 +1,120 @@
+//Datenbank verlinkung
+
+//Pausebutton
+/* 
+	1.Anhalten der Zeit bzw. Datum
+	2.Anhalten der Aktualisierung der Karte
+*/
+
+//Straßen
+//Heerstraßen
+/*
+	1.der Button ist nur anklickbar wenn die Anzhal der Arbeiter und nötige Bauelemente dazu passen.
+	2.Auswahl des passenden Bildes von der Gallery
+*/
+
+//Provinzstraßen
+/*
+	1.der Button ist nur anklickbar wenn die Anzhal der Arbeiter und nötige Bauelemente dazu passen.
+	2.Auswahl des passenden Bildes von der Gallery
+*/
+
+
+//Privatstraßen
+/*
+	1.der Button ist nur anklickbar wenn die Anzhal der Arbeiter und nötige Bauelemente dazu passen.
+	2.Auswahl des passenden Bildes von der Gallery
+*/
+
+//Städtische Straßen
+/*
+	1.der Button ist nur anklickbar wenn die Anzhal der Arbeiter und nötige Bauelemente dazu passen.
+	2.Auswahl des passenden Bildes von der Gallery
+*/
+
+//Bauelemente
+//Lehm
+/*
+	1.der Button ist nur anklickbar wenn genüg Lehm vorhanden ist und wenn die ausgewählte Straßen Lehm als Bauelement benötigt.
+	2.Abzug eines Lehmwagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//Kalkstein mit Mörtel
+/*
+	1.der Button ist nur anklickbar wenn genüg Kalkstein mit Mörtel vorhanden ist und wenn die ausgewählte Straßen Kalkstein mit Mörtel als Bauelement benötigt.
+	2.Abzug eines Kalksteinswagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//faustgroßen Kieseln
+/*
+	1.der Button ist nur anklickbar wenn genüg faustgroßen Kieseln vorhanden ist und wenn die ausgewählte Straßen faustgroßen Kieseln als Bauelement benötigt.
+	2.Abzug eines faustgroßen Kieselnswagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//nußgroßen Kieseln
+/*
+	1.der Button ist nur anklickbar wenn genüg nußgroßen Kieseln vorhanden ist und wenn die ausgewählte Straßen nußgroßen Kieseln als Bauelement benötigt.
+	2.Abzug eines nußgroßen Kieselnswagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//Kopfsteinpflaster
+/*
+	1.der Button ist nur anklickbar wenn genüg Kopfsteinpflaster vorhanden ist und wenn die ausgewählte Straßen Kopfsteinpflaster als Bauelement benötigt.
+	2.Abzug eines Kopfsteinpflasterwagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//Steinplatten
+/*
+	1.der Button ist nur anklickbar wenn genüg Steinplatten vorhanden ist und wenn die ausgewählte Straßen Steinplatten als Bauelement benötigt.
+	2.Abzug eines Steinplattenwagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//Kies
+/*
+	1.der Button ist nur anklickbar wenn genüg Kies vorhanden ist und wenn die ausgewählte Straßen Kies als Bauelement benötigt.
+	2.Abzug eines Kieswagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//Sand
+/*
+	1.der Button ist nur anklickbar wenn genüg Sand vorhanden ist und wenn die ausgewählte Straßen Sand als Bauelement benötigt.
+	2.Abzug eines Sandwagens von der Datenbank und von der Bauelementliste der Straße.
+	3.wieder zu 1
+*/
+
+//Arbeiter
+//Soldaten
+/*
+	1.der Button ist nur anklickbar wenn genüg Soldaten vorhanden sind
+	2.Abzug 5 Soldaten von der Datenbank.
+	3.wieder zu 1
+*/
+
+//Sklaven
+/*
+	1.der Button ist nur anklickbar wenn genüg Sklaven vorhanden sind
+	2.Abzug 5 Sklaven von der Datenbank.
+	3.wieder zu 1
+*/
+
+//Sträflinge
+/*
+	1.der Button ist nur anklickbar wenn genüg Sträflinge vorhanden sind
+	2.Abzug 5 Sträflinge von der Datenbank.
+	3.wieder zu 1
+*/
+
+//Sklaven der jeweiligen Grundbesitzer
+/*
+	1.der Button ist nur anklickbar wenn genüg Sklaven der jeweiligen Grundbesitzer vorhanden sind
+	2.Abzug 5 Sklaven der jeweiligen Grundbesitzer von der Datenbank.
+	3.wieder zu 1
+*/
+
